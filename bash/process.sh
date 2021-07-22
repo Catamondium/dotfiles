074949c421d552db050513a2525aa4c3d3178147
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2021-07-22 21:57:29 +0100

    rebasing comp

--- bash/process.sh
+++ bash/process.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
 cd ~/Downloads
 
+updir --sync ./stuff,/stuff
 dedupdir -r ./stuff
 find ./stuff -mindepth 2 -type f -exec mogrify -resize x1080\< {} \;
 dedupdir -r ./stuff
 rname -r -f ./stuff
-updir --sync ./stuff,/stuff
+updir --rebase local ./stuff,/stuff
